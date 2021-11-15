e5c94d802f1ef1c87dcc409a08c4e72dbccc5f16
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-11-15 13:16:06 -0600

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Controller from "./comps/Controller";
 import ManagerCont from "./comps/Manager/ManagerCont";
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Controller />
-      <ManagerCont />
+      <Routes>
+        <Route exact path = "/" element={<Controller />}/>
+        <Route path="/manager" element={<ManagerCont/>}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
